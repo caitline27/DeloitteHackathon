2f17fb3704b5ca445c185664f8778113c220a35a
Mengqing Deng <m.deng@mail.utoronto.ca>
2021-01-23 13:05:57 -0500

    some basic style draft

--- FrontEnd/dashboard/src/App.js
+++ FrontEnd/dashboard/src/App.js
@@ -6,7 +6,11 @@ function App() {
   return (
     <div className="container">
       <div className="row dashboard-wrapper pl-5">
-        <h3 className="pt-4 pb-5 text-center w-100">Project Power</h3>
+        <img src="logo.svg" width="60%" height="60%" class="center"/>
+        <nav class="navline">
+          <h3 className="pt-4 pb-5 text-center w-100">Project Power</h3>
+        </nav>
+        
         <Form />
         <RecommendationComp />
       </div>
