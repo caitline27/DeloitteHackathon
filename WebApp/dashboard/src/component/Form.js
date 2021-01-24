f16cfe9f95e7bf71c4a8a6c5fa97ea3b9f2a6737
Marc Lockhead <mlockhead@deloitte.ca>
2021-01-24 18:46:51 -0500

    more ui

--- WebApp/dashboard/src/component/Form.js
+++ WebApp/dashboard/src/component/Form.js
@@ -4,7 +4,8 @@ import ConsumptionGraph from "./ConsumptionGraph"
 import "./Form.css"
 import axios from "axios"
 
-const region = ["Alberta", "Montreal", "British Columbia", "New Brunswick", "Northwest Territories", "Nova Scotia", "Ontario", "Quebec"];
+
+const region = ["Bergen","Kirstiansund","Oslo","Tromso","Tronheim"];
 // const testGraphData = [
 //   { label: "Jan",  y: 10  },
 //   { label: "Feb", y: 15  },
@@ -27,11 +28,11 @@ class Form extends Component {
     super(props)
     this.state = {
       city: '',
-      start_date: '',
-      end_date: '',
+      date: '',
       isGraphVisible: false,
       testGraphData: [],
-      reserving_value: 0
+        reserving_watt_value: "---",
+      loading : true
     }
   }
 
@@ -45,85 +46,108 @@ class Form extends Component {
       this.setState({ [name]: value })
     }
 
-  }
+    }
+
+    
+    
+
 
 
   formSubmit = (event) => {
     event.preventDefault();
-    const { city, start_date, end_date } = this.state;
+    const { city, date} = this.state;
 
     const formData = {
       city: city,
-      start_date: start_date,
-      end_date: end_date
+      date: date,
     }
     const header = {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
     }
 
-    if (city.length > 0 && start_date.length > 0 && end_date.length > 0) {
+    if (city.length > 0 && date.length > 0 ) {
 
+      
+          axios.post("http://127.0.0.1:5000/reserve", formData, { headers: header }).then(res => {
 
-      axios.post("http://127.0.0.1:5000/reserve", formData, { headers: header }).then(res => {
-
-        if (res) {
-          const { data, reserving_value } = res["data"];
-          this.setState({ "testGraphData": data, reserving_value, isGraphVisible: true })
-        }
-      });
+            if (res) {
+              const { data, reserving_watt_value } = res["data"];
+              this.setState({ "testGraphData": data, reserving_watt_value, isGraphVisible: true })
+            }
+          });
     }else{
       alert("Please enter all the fields")
     }
 
   }
 
-  render() {
-    const { start_date, end_date, city, isGraphVisible } = this.state;
-    return (
-      <>
-        <div className="col-12">
+    render() {
 
-          <p className="instruction mb-4">Select the date to see our ML prediction of how much electricity you should buy.</p>
+        let styleSpinner = {
+                        width: '70px',
+                        height: '70px',
+                    };
 
-          <div className="col-4 float-left">
-            <form onSubmit={this.formSubmit} noValidate>
 
-              <div className="mb-4">
-                <select className="form-control col-10" value={city} onChange={this.handleChange}>
+        const aSpinner = (
 
-                  <option value="">Choose your region</option>
-                  {region.map(ele => <option key={ele} value={ele}>{ele}</option>)}
-                </select>
-              </div>
-              <div className="mb-4">
-                <input type="date" className="form-control col-10" name="start_date" value={start_date} placeholder="start date" onChange={this.handleChange} />
-              </div>
-              <div className="mb-4">
-                <input type="date" className="form-control col-10" name="end_date" value={end_date} placeholder="start date" onChange={this.handleChange} />
-              </div>
-              <div className="mb-4">
-                <div className="col-10 d-flex justify-content-center">
-                  <button className="btn btn-info btn-75">Check</button>
+            
+            <div class="d-flex justify-content-center align-items-center" >
+                <div class="spinner-border" style={ styleSpinner} role="status">
+                    
                 </div>
-              </div>
-            </form>
-          </div>
-          <div className="col-8 float-left">
-
-            <p className="watt-value">{this.state.reserving_value}</p>
-
-          </div>
-        </div>
-
-        {isGraphVisible &&
-          <div className="col-12 pb-5">
-            <div className="col-12 graph mx-auto graphContainer" >
-              <ConsumptionGraph dataPoints={this.state.testGraphData} />
             </div>
-          </div>
-        }
-      </>
+        );  
+
+
+
+
+    const { date,  city, isGraphVisible } = this.state;
+    return (
+        <div className="container" >
+            <div class="row">
+                <p className="instruction mb-4">Select the date to see our ML prediction of how much electricity you should buy.</p>
+            </div>
+
+            <div class="row d-flex align-items-center">
+                <div className="col-4">
+                    <form onSubmit={this.formSubmit} noValidate>
+
+                        <div className="mb-4">
+                            <select className="form-control col-10" value={city} onChange={this.handleChange}>
+
+                                <option value="">Choose your region</option>
+                                {region.map(ele => <option key={ele} value={ele}>{ele}</option>)}
+                            </select>
+                        </div>
+                        <div className="mb-4">
+                            <input type="date" className="form-control col-10" name="date" value={date} placeholder="Date" onChange={this.handleChange} />
+                        </div>
+                        <div className="mb-4">
+                            <div className="col-10 d-flex justify-content-center">
+                                <button className="btn btn-info btn-75">Check</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+
+                <div className="col-8">
+                    {this.state.loading ? aSpinner : <div> <p className="watt-value">{this.state.reserving_watt_value}</p></div>}
+                </div>
+
+            </div>
+
+            {isGraphVisible &&
+                <div className="col-12 pb-5">
+                    <div className="col-12 graph mx-auto graphContainer" >
+                        <ConsumptionGraph dataPoints={this.state.testGraphData} />
+                    </div>
+                </div>
+            }
+
+      </div>
     )
 
 
