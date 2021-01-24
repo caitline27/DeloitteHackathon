41259a4fa4f0b71d6c3ff3e49a11eff923e61494
mjan <mjan@CAW180042>
2021-01-23 21:08:57 -0500

    -renamed the app folder  to webapp

---
+++ WebApp/dashboard/src/component/Form.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+
+import ConsumptionGraph from "./ConsumptionGraph"
+import "./Form.css"
+import axios from "axios"
+
+const region = ["Alberta", "Montreal", "British Columbia", "New Brunswick", "Northwest Territories", "Nova Scotia", "Ontario", "Quebec"];
+// const testGraphData = [
+//   { label: "Jan",  y: 10  },
+//   { label: "Feb", y: 15  },
+//   { label: "Mar", y: 25  },
+//   { label: "Apr",  y: 30  },
+//   { label: "May",  y: 28  },
+//   { label: "Jun",  y: 10  },
+//   { label: "Jul", y: 15  },
+//   { label: "Aug", y: 25  },
+//   { label: "Sep",  y: 30  },
+//   { label: "Oct",  y: 28  },
+//   { label: "Nov",  y: 56  },
+//   { label: "Dec",  y: 28  }
+// ]
+
+class Form extends Component {
+
+  constructor(props) {
+
+    super(props)
+    this.state = {
+      city: '',
+      start_date: '',
+      end_date: '',
+      isGraphVisible: false,
+      testGraphData: [],
+      reserving_value: 0
+    }
+  }
+
+  handleChange = event => {
+
+    if (event.target.tagName.toUpperCase() === "SELECT") {
+      const city = event.target.value;
+      this.setState({ city })
+    } else {
+      const { name, value } = event.target;
+      this.setState({ [name]: value })
+    }
+
+  }
+
+
+  formSubmit = (event) => {
+    event.preventDefault();
+    const { city, start_date, end_date } = this.state;
+
+    const formData = {
+      city: city,
+      start_date: start_date,
+      end_date: end_date
+    }
+    const header = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+
+    if (city.length > 0 && start_date.length > 0 && end_date.length > 0) {
+
+
+      axios.post("http://127.0.0.1:5000/reserve", formData, { headers: header }).then(res => {
+
+        if (res) {
+          const { data, reserving_value } = res["data"];
+          this.setState({ "testGraphData": data, reserving_value, isGraphVisible: true })
+        }
+      });
+    }else{
+      alert("Please enter all the fields")
+    }
+
+  }
+
+  render() {
+    const { start_date, end_date, city, isGraphVisible } = this.state;
+    return (
+      <>
+        <div className="col-12">
+
+          <h5 className="mb-4">How much electricity should you buy?</h5>
+
+          <div className="col-4 float-left">
+            <form onSubmit={this.formSubmit} noValidate>
+
+              <div className="mb-4">
+                <select className="form-control col-10" value={city} onChange={this.handleChange}>
+
+                  <option value="">Choose your region</option>
+                  {region.map(ele => <option key={ele} value={ele}>{ele}</option>)}
+                </select>
+              </div>
+              <div className="mb-4">
+                <input type="date" className="form-control col-10" name="start_date" value={start_date} placeholder="start date" onChange={this.handleChange} />
+              </div>
+              <div className="mb-4">
+                <input type="date" className="form-control col-10" name="end_date" value={end_date} placeholder="start date" onChange={this.handleChange} />
+              </div>
+              <div className="mb-4">
+                <div className="col-10 d-flex justify-content-center">
+                  <button className="btn btn-info btn-75">Check</button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="col-8 float-left">
+
+            <p className="watt-value">{this.state.reserving_value} MW</p>
+
+          </div>
+        </div>
+
+        {isGraphVisible &&
+          <div className="col-12 pb-5">
+            <div className="col-12 graph mx-auto graphContainer" >
+              <ConsumptionGraph dataPoints={this.state.testGraphData} />
+            </div>
+          </div>
+        }
+      </>
+    )
+
+
+
+  }
+
+
+
+}
+
+
+export default Form;
---
+++ WebApp/dashboard/src/component/RecomComponent.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import {  IgrRadialGauge } from 'igniteui-react-gauges';
+import Gauge from './Gauge.js';
+
+const region = ["Alberta", "Montreal", "British Columbia", "New Brunswick", "Northwest Territories", "Nova Scotia", "Ontario", "Quebec"];
+class RecomComponent extends Component {
+
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            gaugeValue: 50,
+            BuySell: '',
+            tradingAmount: 0,
+            region : ''
+        };
+    }
+
+    modifyRadioState =(event) => {
+        console.log(event.target);
+
+        this.setState({
+            "BuySell" : event.target.id
+        });
+    }
+
+    modifyRegion = (event) => {
+        this.setState({
+            "region": event.target.value
+        });
+    }
+
+    modifyAmount = (event) =>{
+        this.setState({
+            "tradingAmount": event.target.value
+        });
+    }
+
+    newGaugeValue = (event) => {
+        this.setState({
+            "gaugeValue": event.value
+        });
+    }
+ 
+
+   
+    render() {
+
+        return (
+            <div class="container border">
+
+
+                <h5>What would you like to do?</h5>
+
+                <div class="row align-items-center">
+                    <div class="col-6 ">
+                        <div class="row justify-content-center" >
+                            <input type="radio" id="buy" name="price" value="buy" onChange={this.modifyRadioState} />
+                            <label htmlFor="buy" className="pr-5 pl-2">Buy</label>
+                            
+                            <input type="radio" id="sell" name="price" value="sell" onChange={this.modifyRadioState} />
+                            <label htmlFor="sell" className="pl-2">Sell</label>
+                        </div>
+                        <div class="row justify-content-center" onChange={this.modifyAmount}>
+                            <input id="amount" type="text" className="form-control col-5 float-left" placeholder="Enter the amount you would like to trade" />
+                        </div>
+                        <div class="row justify-content-center" onChange={this.modifyRegion}>
+                            <select className="form-control col-3 float-left mr-5">
+                                <option value="">Choose your region</option>
+                                {region.map(ele => <option id="region" key={ele} value={ele}>{ele}</option>)}
+                            </select>
+                        </div>
+                    </div>
+                    
+                    <div class="col-6">
+                        <h6>What is your preference?</h6>    
+              
+                        <IgrRadialGauge
+                            value={this.state.gaugeValue}
+                            isNeedleDraggingEnabled={true}
+                            isNeedleDraggingConstrained={true}
+                            needleBrush="DodgerBlue"
+                            needleOutline="DodgerBlue"
+                            needleEndExtent={0.475}
+                            needleStrokeThickness={1}
+                            needlePivotShape="CircleOverlay"
+                            needlePivotBrush="#9f9fa0"
+                            needlePivotOutline="#9f9fa0"
+                            needlePivotWidthRatio={0.2}
+                            needlePivotStrokeThickness={1}
+                            height="300px" width="300px"
+                            minimumValue={0}
+                            maximumValue={100} interval={10}
+                            valueChanged={this.newGaugeValue}/>
+                        <p>someletter: {this.state.gaugeValue}</p>
+                    </div>       
+                </div>
+
+
+
+
+
+                <div class="row">
+                    <div class="col-6">
+                        <button class="btn btn-info btn-75" onClick={this.sendToAPI}>Check</button>
+                        
+                    </div>
+                    <div class="col-6">
+                        <div class="d-flex justify-content-start">
+                            <p>Recommended Price :</p>
+                            <p><strong>$20/MW</strong></p>
+                        </div>
+                        <div class="d-flex justify-content-start">
+                            <p>Environmental Score : </p>
+                            <p><strong> 80%</strong></p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        )
+
+    }
+}
+
+
+export default RecomComponent;
