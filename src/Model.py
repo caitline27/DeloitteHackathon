dc5266d71a5802af271195752c6cc58d7dad695c
das <das@baba.baba>
2021-01-25 01:43:32 -0500

    now with some hyper parameters

--- src/Model.py
+++ src/Model.py
@@ -2,7 +2,7 @@ import pandas as pd
 from numpy import loadtxt
 from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, mean_absolute_percentage_error
 
 from src import TimeSeriesCleaning as tsc
 
@@ -31,8 +31,11 @@ def main():
                                                         random_state=seed)
 
     # fit model no training data
-    model = XGBRegressor()
+    model = XGBRegressor(n_estimators=1000, max_depth=12, learning_rate=0.1, min_child_weight=6)
+    # model = XGBRegressor()
     model.fit(X_train, y_train)
+
+    print(X_cols)
 
     # make predictions for test data
     y_pred = model.predict(X_test)
@@ -44,17 +47,22 @@ def main():
     results_df['diff'] = results_df['prediction'] - results_df['actuals']
     results_df['percent'] = results_df['diff']/results_df['actuals']
     print( f"mean is {abs(results_df['percent']).mean()}" )
-    print(results_df['percent'].describe())
+    print( f"max is {max(results_df['percent'])}" )
+    print( f"min is {min(results_df['percent'])}" )
 
     # evaluate predictions
     error = mean_squared_error(y_test, y_pred)
     print("Error: ", error**.5)
 
+    print(f"mean abs error {mean_absolute_error(y_test, y_pred)}")
+
+    print(f"mean abs percent error {mean_absolute_percentage_error(y_test, y_pred)}")
 
     r2 = r2_score(y_test, y_pred)
     print("r2: ", r2)
     print(sorted(list(zip(X_cols, model.feature_importances_)),
                  key = lambda x: x[1],
-                 reverse = True)[:10])
+                 reverse = True)[:])
+
 if __name__ == '__main__':
     main()
