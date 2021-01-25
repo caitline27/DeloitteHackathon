050767e7a6e42efaf123d48f9b1e1155a1be59a8
Mengqing Deng <m.deng@mail.utoronto.ca>
2021-01-25 00:04:31 -0500

    merge by hand

--- WebApp/dashboard/src/component/Form.js
+++ WebApp/dashboard/src/component/Form.js
@@ -41,149 +41,151 @@ function autoFitFontSize() {
 
 class Form extends Component {
 
-    constructor(props) {
+  constructor(props) {
 
-        super(props)
-        this.state = {
-            region: '',
-            date: '',
-            isGraphVisible: false,
-            factor_ratings: [],
-            reserving_value: "-",
-            loading: false
-        }
+    super(props)
+    this.state = {
+      region: '',
+      date: '',
+      isGraphVisible: false,
+      factor_ratings: [],
+      reserving_value: "0",
+      loading: false
+    }
+  }
+
+  handleChange = event => {
+
+    if (event.target.tagName.toUpperCase() === "SELECT") {
+      const region = event.target.value;
+      this.setState({ region })
+    } else {
+      const { name, value } = event.target;
+      this.setState({ [name]: value })
     }
 
-    handleChange = event => {
+  }
 
-        if (event.target.tagName.toUpperCase() === "SELECT") {
-            const region = event.target.value;
-            this.setState({ region })
-        } else {
-            const { name, value } = event.target;
-            this.setState({ [name]: value })
-        }
 
+  formSubmit = (event) => {
+    event.preventDefault();
+    const { region, date } = this.state;
+
+    const formData = {
+      region: region,
+      date: date
+    }
+    const header = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
     }
 
+    if (region.length > 0 && date.length > 0) {
 
-    formSubmit = (event) => {
-        event.preventDefault();
-        const { region, date } = this.state;
+      axios.post("http://127.0.0.1:5000/reserve", formData, { headers: header }).then(res => {
 
-        const formData = {
-            region: region,
-            date: date
+        if (res) {
+          const { factor_ratings, reserving_value } = res["data"];
+          this.setState({ "factor_ratings": factor_ratings, reserving_value, isGraphVisible: true })
+          autoFitFontSize();
         }
-        const header = {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
-
-        if (region.length > 0 && date.length > 0) {
-
-            axios.post("http://127.0.0.1:5000/reserve", formData, { headers: header }).then(res => {
-
-                if (res) {
-                    const { factor_ratings, reserving_value } = res["data"];
-                    this.setState({ "factor_ratings": factor_ratings, reserving_value, isGraphVisible: true })
-                    autoFitFontSize();
-                }
-            });
+      });
 
 
-        } else {
-            alert("Please enter all the fields")
-        }
-
+    } else {
+      alert("Please enter all the fields")
     }
 
-    render() {
-        const { date, region, isGraphVisible } = this.state;
-        window.onresize = function () { autoFitFontSize() };
+  }
+
+  render() {
+    const { date, region, isGraphVisible } = this.state;
+    window.onresize = function () { autoFitFontSize() };
 
 
-        let styleSpinner = {
-            width: '70px',
-            height: '70px',
-        };
+    let styleSpinner = {
+      width: '70px',
+      height: '70px',
+    };
 
 
-        const aSpinner = (
+    const aSpinner = (
 
 
-            <div class="d-flex justify-content-center align-items-center m-3" >
-                <div class="spinner-border" style={styleSpinner} role="status">
+      <div class="d-flex justify-content-center align-items-center m-3" >
+        <div class="spinner-border" style={styleSpinner} role="status">
 
-                </div>
-            </div>
-        );
+        </div>
+      </div>
+    );
 
 
-        const wattValue = (
-            <div className="col-8">
+    const wattValue = (
+        <div className="watt-text">
+          <p className="watt-value" id="watt">{this.state.reserving_value}</p>
+        </div>
+    )
+
+
+
+
+    return (
+      <>
+        <div className="col-12">
+
+          <p className="instruction delimiter">Select the date and city to see our ML prediction of how much electricity you should buy.</p>
+
+          <div class="container">
+            <div class="row">
+              <div className="col-4">
+                <form onSubmit={this.formSubmit} noValidate>
+
+                  <div className="mb-4">
+                    <select className="form-control col-10" value={region} onChange={this.handleChange}>
+
+                      <option value="">Choose your region</option>
+                      {region_options.map(ele => <option key={ele} value={ele}>{ele}</option>)}
+                    </select>
+                  </div>
+                  <div className="mb-4">
+                    <input type="date" className="form-control col-10" name="date" value={date} placeholder="date" onChange={this.handleChange} />
+                  </div>
+                  <div className="mb-4">
+                    <div className="col-10 d-flex justify-content-center">
+                      <button className="btn btn-info btn-75">Check</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div className="col-8 watt">
+                {this.state.loading ? aSpinner : wattValue}
+              </div>
+
+              {/* <div className="col-8 watt">
                 <div className="watt-text">
-                    <p className="watt-value">{this.state.reserving_value}</p>
+                  <p className="watt-value" id="watt">{this.state.reserving_value}</p>
                 </div>
+              </div> */}
+
+
             </div>
-        )
+          </div>
+
+        </div>
+
+        {isGraphVisible &&
+          <div className="col-12 pb-5">
+            <div className="col-12 graph mx-auto graphContainer" >
+              <PieChart dataPoints={this.state.factor_ratings} />
+            </div>
+          </div>
+        }
+      </>
+    )
 
 
 
-
-        return (
-            <>
-                <div className="col-12">
-
-                    <p className="instruction mb-4">Select the date to see our ML prediction of how much electricity you should buy.</p>
-
-                    <div class="container">
-                        <div class="row">
-                            <div className="col-4">
-                                <form onSubmit={this.formSubmit} noValidate>
-
-                                    <div className="mb-4">
-                                        <select className="form-control col-10" value={region} onChange={this.handleChange}>
-
-                                            <option value="">Choose your region</option>
-                                            {region_options.map(ele => <option key={ele} value={ele}>{ele}</option>)}
-                                        </select>
-                                    </div>
-                                    <div className="mb-4">
-                                        <input type="date" className="form-control col-10" name="date" value={date} placeholder="date" onChange={this.handleChange} />
-                                    </div>
-                                    <div className="mb-4">
-                                        <div className="col-10 d-flex justify-content-center">
-                                            <button className="btn btn-info btn-75">Check</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                            <div className="col-8 watt">
-                                {this.state.loading ? aSpinner : wattValue}
-                            </div>
-
-
-
-
-                        </div>
-                    </div>
-
-                </div>
-
-                {isGraphVisible &&
-                    <div className="col-12 pb-5">
-                        <div className="col-12 graph mx-auto graphContainer" >
-                            <PieChart dataPoints={this.state.factor_ratings} />
-                        </div>
-                    </div>
-                }
-            </>
-        )
-
-
-
-    }
+  }
 
 
 
